1a09cc08efcb1b9bdbb5cd66ede1633b0db3244a
adeleeeeyyyy <mitsuhaadly@gmail.com>
2024-05-15 20:03:04 +0700

    adit kontol

---
+++ index.js
@@ -0,0 +1,12 @@
+const telebot = require('node-telegram-bot-api')
+const token = "6701500674:AAGEBIKLLDeJirXwcuWMDCI_ADOYJN1NinI"
+const bot = new telebot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const messageText = msg.text;
+
+  if (messageText === '/start') {
+    bot.sendMessage(chatId, 'Welcome to the bot!');
+  }
+});
